bb03b645cc28099ba4ad215398e5bed259b3d7db
Rishikant Patel <38522693+082-Rishikant@users.noreply.github.com>
2022-03-19 18:35:07 +0530

    stuck when sending the from filed data in thunder client as request

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const mongoose=require('mongoose');
 
-// require('dotenv').config();
-// const mongoURI=process.env.MONGO_URI;
+require('dotenv').config();
 
-const mongoURI = "mongodb://localhost:27017/foundit?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const mongoURI = `mongodb://localhost:27017/${process.env.DB_NAME}?readPreference=primary&appname=MongoDB%20Compass&ssl=false`;
 
 const connectToMongo=()=>{
-    mongoose.connect(mongoURI, 
+    mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true}, 
         ()=>{
       console.log("Connected to mongo successfully");
   })
--- routes/item.js
+++ routes/item.js
@@ -1,25 +1,45 @@
-
 const express = require('express');
 const Item = require('../models/Item');
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
 const fetchuser = require('../middlewares/fetchuser');
-const upload_item_middleware = require('../middlewares/upload_item_middleware');
+// const upload = require("../middlewares/upload_image_middleware");
 
 // Json Web Token*********
 const jwt = require('jsonwebtoken'); // Used for generate tokens for security purpose and we will send this token to loggedin user to verify in future that current user loggedin or not
 require('dotenv').config();
 const JWT_secret = process.env.JWT_SECRET_KEY;
 
+// router.use(express.static(__dirname+"./public/"));
+
+
+
+// const multer = require("multer");
+// const path=require('path');
+// const storage = multer.diskStorage({
+//   destination:"./public/images",
+//   image_name:(req, cb)=>{
+//       cb(null, file.fieldname+"_"+Date.now()+path.extname(file.originalname));
+//   }
+// });
+
+// const upload=multer({
+//   storage:storage
+// }).single('image');
+
+
+
 // Router 1) - Upload an Item using POST:'/api/item/uploaditem' | login required
 router.post('/uploaditem', [
   body('name').isLength({ min: 3 }),
   body('place').isLength({ min: 3 }),
-  fetchuser,
-  // upload_item_middleware.image_upload, 
+  fetchuser
 ],
-  async (req, res) => {
+// upload,
+  async (req, res, next) => {
     let success = false;
+
+    // var image_name=req.file.fieldname;
 
     // JS validator
     const errors = validationResult(req);
@@ -28,14 +48,20 @@ router.post('/uploaditem', [
     }
 
     // create an new item using Item model
-    const { name, type, date, place, description } = req.body;
-    const item = new Item({ user: req.user_id, name, type, date, place, description });
+    const item = new Item({ 
+      user: req.user_id, 
+      name:req.body.name, 
+      type:req.body.type, 
+      date:req.body.date, 
+      place:req.body.place, 
+      description:req.body.description
+    });
     success = true;
 
     // Now save the item to mongodb
     const savedItem=await item.save();
 
-    res.json({ success, savedItem });
+    res.send({ success, savedItem });
   });
 
 
