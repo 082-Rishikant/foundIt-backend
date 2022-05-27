001334b0b2819a6b6ce45ea73853a26155ec4caf
Rishikant Patel <38522693+082-Rishikant@users.noreply.github.com>
2022-05-27 05:50:51 +0530

    Completed till update Item, Now only viewItem component is left

--- routes/item.js
+++ routes/item.js
@@ -17,7 +17,7 @@ const JWT_secret = process.env.JWT_SECRET_KEY;
 
 
 
-// Router- 1 Upload an Item using POST:'/api/item/uploaditem' | login required**********
+// Router- 1 ADD an Item using POST:'/api/item/uploaditem' | login required**********
 
 // ***multer function for middleware***
 const storage = multer.diskStorage({
@@ -31,12 +31,7 @@ const storage = multer.diskStorage({
 })
 
 // ***multer middleware***
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5
-  }
-});
+const upload = multer({storage: storage, limits: {fileSize: 1024 * 1024 * 5}});
 
 
 router.post('/uploaditem',
@@ -54,10 +49,10 @@ router.post('/uploaditem',
     if (!errors.isEmpty()) {
       fs.unlink(req.file.path, (err) => {
         if (err) {
-          return res.status(501).json({ success:false, message: "delete a just stored file when textfield is not valid", errors: err });
+          return res.status(501).json({ success: false, message: "delete a just stored file when textfield is not valid", errors: err });
         }
       })
-      return res.status(502).json({ success:false, message:"Some errors in creds validation", errors: errors.array() });
+      return res.status(502).json({ success: false, message: "Some errors in creds validation", errors: errors.array() });
     }
 
     try {
@@ -65,18 +60,18 @@ router.post('/uploaditem',
       const image_name = req.file.filename;
 
       // create an new item using Item model
-      let date=new Date();
-      if(req.body.date){
-        date=new Date(req.body.date);
+      let date = new Date();
+      if (req.body.date) {
+        date = new Date(req.body.date);
       }
       const item = new Item({
         user: req.user_id,
         name: req.body.name,
         type: req.body.type,
         date: date,
-        day:date.getDate(),
-        month:date.getMonth()+1,
-        year:date.getFullYear(),
+        day: date.getDate(),
+        month: date.getMonth() + 1,
+        year: date.getFullYear(),
         place: req.body.place,
         description: req.body.description,
         image_name: image_name
@@ -84,9 +79,9 @@ router.post('/uploaditem',
       // Now save the item to mongodb
       const savedItem = await item.save();
 
-      res.send({ success:true, savedItem });
+      res.send({ success: true, savedItem });
     } catch (error) {
-      return res.status(503).send({success:false, message:"Catch section",  error: error.message });
+      return res.status(503).send({ success: false, message: "Catch section", error: error.message });
     }
   });
 
@@ -119,18 +114,18 @@ router.post('/searchItem',
       // const day=d.getDate();
 
       // Fetch all items those matches
-      let items_list = await Item.find({$or:[{ name: name, type: type, place:place}]});
-      
+      let items_list = await Item.find({ $or: [{ name: name, type: type, place: place }] });
+
       // update them with user details
       for (var rep = 0; rep < items_list.length; rep++) {
-        let user_id=items_list[rep].user;
+        let user_id = items_list[rep].user;
         let uploader_user = await User.findById(user_id).select("name mobile_no user_image department");
         items_list[rep].set('user', uploader_user);
       }
 
-      res.send({success:true, items_list });
+      res.send({ success: true, items_list });
     } catch (error) {
-      res.status(500).send({success:false, message: error.message });
+      res.status(500).send({ success: false, message: error.message });
     }
   });
 
@@ -141,27 +136,108 @@ router.delete('/deleteItem/:id', fetchuser, async (req, res) => {
     // checking whether item exist or not, If find then delete
     let item = await Item.findById(req.params.id);
     if (!item) {
-      return res.status(400).send({success:false, message:"Item not found that you want to delete"});
+      return res.status(400).send({ success: false, message: "Item not found that you want to delete" });
     }
 
     // checking whether user owns this item or not
     if (item.user.toString() !== req.user_id) {
-      return res.status(401).send({success:false, message:"sorry!! You are not allowed to delete this item"});
+      return res.status(401).send({ success: false, message: "sorry!! You are not allowed to delete this item" });
     }
     // Finaly deleting Image**
     fs.unlink(`./public/item_Images/${item.image_name}`, (err) => {
       if (err) {
-        return res.status(501).json({ success:false, message2: "sorry! Not able to delete a image", message: err });
+        return res.status(501).json({ success: false, message2: "sorry! Not able to delete a image", message: err });
       }
     })
     // finaly Deleting item**
     item = await Item.findByIdAndDelete(req.params.id);
-    res.json({success:true,message:"Item deleted successfully",item:item});
+    res.json({ success: true, message: "Item deleted successfully", item: item });
   } catch (error) {
     console.error(error.message);
-    res.status(402).send({success:false, message:error.message, message2:"catch"});
+    res.status(402).send({ success: false, message: error.message, message2: "catch" });
   }
 })
+
+
+// Router 5: Update an existing Item using:PUT   '/api/item/updateItem/:id'   Login required;
+router.put('/updateItem/:id',
+  fetchuser,
+  upload.single('image'), [
+  // validation rules for input
+  body('name', 'Enter a valid item name').isLength({ min: 2 }),
+  body('place', 'Enter a valid item name').isLength({ min: 2 })
+],
+  async (req, res, next) => {
+    //check for validaion errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      fs.unlink(req.file.path, (err) => {
+        if (err) {
+          return res.status(501).json({ success: false, message: "delete a just stored file when textfield is not valid", errors: err });
+        }
+      })
+      return res.status(502).json({ success: false, message: "Some errors in creds validation", errors: errors.array() });
+    }
+    
+    try {
+      const { name, type, place, date, description } = req.body;
+      const image_name = req.file.filename;
+      
+      // Making a item that will replace old existing item
+      const newItem = {};
+      if (name) newItem.name = name;
+      if (type) newItem.type = type;
+      if (place) newItem.place = place;
+      if (date){
+        ndate = new Date(date);
+        newItem.date = ndate;
+        newItem.day = ndate.getDate();
+        newItem.month = ndate.getMonth() + 1;
+        newItem.year = ndate.getFullYear();
+      }
+      if (description) newItem.description = description;
+      if (image_name) newItem.image_name = image_name;
+      
+      // checking whether item exist or not, If exist then Update it
+      let item = await Item.findById(req.params.id);
+      if (!item) {
+        fs.unlink(req.file.path, (err) => {
+          if (err) {
+            return res.status(501).json({ success: false, message: "delete a just stored file when textfield is not valid", errors: err });
+          }
+        })
+        return res.status(404).send({ success: false, message: "Item not found to update" });
+      }
+
+      // checking whether user owns this Item or not
+      if (item.user.toString() !== req.user_id) {
+        fs.unlink(req.file.path, (err) => {
+          if (err) {
+            return res.status(501).json({ success: false, message: "delete a just stored file when textfield is not valid", errors: err });
+          }
+        })
+        return res.status(401).send({ success: false, message: "You are not allowed to update this Item" });
+      }
+
+      // Finaly deleting Image**
+      fs.unlink(`./public/item_Images/${item.image_name}`, (err) => {
+        if (err) {
+          return res.status(501).json({ success: false, message2: "sorry! Not able to delete old image a image", message: err });
+        }
+      })
+
+      // Finaly updating in mongo DB**
+      item = await Item.findByIdAndUpdate(req.params.id, { $set: newItem }, { new: true })
+      res.json({success:true, item});
+    } catch (error) {
+      fs.unlink(req.file.path, (err) => {
+        if (err) {
+          return res.status(501).json({ success: false, message: "delete a just stored file when textfield is not valid", errors: err });
+        }
+      })
+      res.status(500).send({success:false, message:error.message, message2:"Catch section"});
+    }
+  })
 
 
 module.exports = router;
